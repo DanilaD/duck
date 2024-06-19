8f06758e5882b5b31360b9dc1562f5b963619fe8
Danila Dolmatov <d6174173844@gmail.com>
2024-06-19 11:43:26 -0400

    set default connection for mongodb

--- config/database.php
+++ config/database.php
@@ -94,14 +94,13 @@ return [
         ],
 
         'mongodb' => [
-            'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-            'options' => [
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+            'driver'   => 'mongodb',
+            'dsn'      => env('DB_DSN'),
+            'database' => env('DB_DATABASE', 'duck'),
+            'username' => env('DB_USERNAME', 'admin'),
+            'password' => env('DB_PASSWORD', 'password'),
+            'options'  => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin')
             ]
         ],
 
